51d88de7b646244bfc406b699e71f1e15bbe1e19
Nathan Clevenger <nateclev@gmail.com>
2022-09-13 08:20:50 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,3 +1,17 @@
+const api = {
+  icon: '⚡️☁️',
+  name: 'webhooks.do',
+  description: 'Webhook Storage',
+  url: 'https://webhooks.do/api',
+  type: 'https://apis.do/tools',
+  endpoints: {
+    webhook: 'https://webhooks.do/:namespace',
+    details: 'https://webhooks.do/:namespace/:id',
+  },
+  site: 'https://webhooks.do',
+  repo: 'https://github.com/drivly/webhooks.do',
+}
+
 export default {
   fetch: async (req, env) =>  {
     return new Response(JSON.stringify({ api, user }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
